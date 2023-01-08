5fa142714a61be3c80abfacc9f068a43d4c56ad2
Oren Shalev <orenico4@gmail.com>
2023-01-08 07:54:54 +0200

    updated lient url to use the amazon serverless

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
+  uri: 'https://5142s291u6.execute-api.us-east-1.amazonaws.com/',
   cache: new InMemoryCache(),
 });
 
